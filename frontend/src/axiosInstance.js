87c39639e11308cf18fa4a48b65d36b25903ace9
MrD0511 <sharmadhruv00005@gmail.com>
2024-10-06 18:02:48 +0530

    lowered python version

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://13.60.227.234:8000',
+    baseURL: 'http://127.0.0.1:8000',
 });
 
 axiosInstance.interceptors.request.use(
