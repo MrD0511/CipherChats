cb51d823db8b976818e0dff60e2b2f5e5093f648
MrD0511 <sharmadhruv00005@gmail.com>
2024-10-06 19:03:39 +0530

    fixing bug

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000',
+    baseURL: 'http://16.171.133.154:8000',
 });
 
 axiosInstance.interceptors.request.use(
